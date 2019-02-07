fbf326d011475cfd2c2d8dd33a9102f39f6bcabd
Nicholas Walton <ngwalton@gmail.com>
2019-02-07 01:04:53 -0600

    Commenting

--- analysis/block_evidence.R
+++ analysis/block_evidence.R
@@ -16,20 +16,25 @@ setwd(here::here("data"))
 print_map <- TRUE
 
 
+# output files ----
+
 # name of output shapefile without extension
 out_shp <- "evidence_by_block"
 
 # name of output pdf file if printing maps
 out_pdf <- "evidence_maps.pdf"
 
+
+# load data ----
+
 # birdpop alpha codes;
 # common names are in "COMMONNAME", and 4-letter alpha codes are in "SPEC"
 # source: http://www.birdpop.org/pages/birdSpeciesCodes.php
 alpha <- read.dbf("LIST18.DBF", as.is = TRUE)
 
-# arguments for readOGR are input format dependent;
-# with a shapefile, the first argument is the directory containing the shp,
-# and the second argument is the name of the shapefile without the extension
+# block shapefile; arguments for readOGR are input format dependent -- with a
+# shapefile, the first argument is the directory containing the shp, and the
+# second argument is the name of the shapefile without the extension
 block_in <- readOGR("blk", "WbbaBlocks2015_v0_2")
 
 # optional county layer --  only used for map printing
@@ -37,6 +42,9 @@ cnty <- readOGR("county", "County_Boundaries_24K")
 
 # sample WBBA data from ebrid
 sp_in <- read.csv("wbba2018.csv", as.is = TRUE)
+
+
+# data prep ----
 
 # remove hybrid, spuh, and slash taxonomic categories
 taxa <- c("species", "issf", "domestic", "form")
@@ -59,17 +67,24 @@ sp_wgs <- SpatialPointsDataFrame(sp_in[, c("LONGITUDE", "LATITUDE")], sp_in,
 nad83 <- CRS(proj4string(block_in))  # use NAD83 from block_in
 sp_nad <- spTransform(sp_wgs, nad83)
 
-# extract blocks that overlay points;
-# returns a data frame containing the same number rows as sp_nad;
-# each row is a record from block that overlays the points in sp_nad
+# extract blocks that overlay points; returns a data frame containing the same
+# number rows as sp_nad; each row is a record from block that overlays the
+# points in sp_nad
 block_over <- over(sp_nad, block_in)
 names(block_over)[13] <- "CO_eBird"  # COUNTY is in both data frames
 
 # ...and join them to the bird data frame
 sp <- cbind(sp_nad@data, block_over)
 
+# some of the BREEDING.BIRD.ATLAS.CODE codes have a space at the end
+# and some don't - this removes the space
+sp$BREEDING.BIRD.ATLAS.CODE <- trimws(sp$BREEDING.BIRD.ATLAS.CODE)
+
 # add column for breeding evidence code
 sp$conf <- 0
+
+
+# add evidence codes ----
 
 # list of lists containing the breeding codes;
 # numbers are used at first instead of names to simply finding the highest
@@ -82,24 +97,20 @@ breeding_codes <- list(
                          "FY", "FS", "NE", "NY"))
 )
 
-# some of the BREEDING.BIRD.ATLAS.CODE codes have a space at the end
-# and some don't - this removes the space
-sp$BREEDING.BIRD.ATLAS.CODE <- trimws(sp$BREEDING.BIRD.ATLAS.CODE)
-
 # assign numeric breeding code (1 = lowest, 4 = highest)
 for (code in breeding_codes) {
   sp$conf[sp$BREEDING.BIRD.ATLAS.CODE %in% code[[3]]] <- code[[1]]
 }
 
-# function to assign breeding code name;
-# for a given alpha code/block combo, all values of "conf" are passed to this
-# function which returns the highest breeding evidence name
+# function to assign breeding code name; for a given alpha code/block combo, all
+# values of "conf" are passed to this function which returns the highest
+# breeding evidence name
 code_name <- function(x){
   code_num <- max(x)
   breeding_codes[[code_num]][[2]]
 }
 
-# this creates a data frame with BLOCK_ID as the 1st column, followed by
+# create a data frame with BLOCK_ID as the 1st column, followed by
 # columns for each alpha code, with breeding evidence name as the cell values
 sp_cast <- dcast(sp, BLOCK_ID ~ SPEC, fun.aggregate = code_name,
                  fill = "", value.var = "conf")
@@ -108,8 +119,10 @@ sp_cast <- dcast(sp, BLOCK_ID ~ SPEC, fun.aggregate = code_name,
 block_out <- merge(block_in, sp_cast, by = "BLOCK_ID")
 
 
-# print maps
-# print an evidence map for each species to a single pdf
+# print maps ----
+
+# print an evidence map for each species to a single pdf; note that this can be
+# time consuming
 
 if (print_map) {
   block_map <- block_out
@@ -132,7 +145,6 @@ if (print_map) {
   # open pdf device
   pdf(out_pdf)
 
-  # note that this can be time consuming
   for (i in seq_along(taxa)) {
     if (i == 1) message(paste("Printing", n, "maps"))
 
@@ -155,6 +167,6 @@ if (print_map) {
   dev.off()
 }
 
-# write to disc as a shapefile
+# write to disc as a shapefile ---
 
 writeOGR(block_out, ".", "out_shp", driver = "ESRI Shapefile")
