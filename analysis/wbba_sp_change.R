51d874b1ea8334b64800a1da900551d18a732ce1
Nick Anich <47158373+nmanich@users.noreply.github.com>
2021-11-24 12:47:00 -0600

    remove domestics, keep pigeon

--- analysis/wbba_sp_change.R
+++ analysis/wbba_sp_change.R
@@ -57,10 +57,14 @@ cnty <- us_boundaries(type = "county", resolution = "high", states = "WI")
 sp$ii <- subset(sp$ii, APPROVED != "0")
 sp$i <- subset(sp$i, APPROVED != "0")
 
+# flag the pigeon entries so they are not removed with the rest of the domestics
+sp$ii <- transform(sp$ii, CATEGORY = ifelse(COMMON.NAME == "Rock Pigeon", "pigeon", CATEGORY))
+sp$i <- transform(sp$i CATEGORY = ifelse(COMMON.NAME == "Rock Pigeon", "pigeon", CATEGORY))
+
 # update_sp removes non-species taxa (i.e., hybrid, spuh, domestic, and slash taxonomic
 # categories), removes records with no breeding evidence (i.e., F, NA, and the
 # empty string), and adds an alpha code (for column naming).
-taxa <- c("species", "issf", "form")
+taxa <- c("species", "issf", "form", "pigeon")
 update_sp <- function(x) {
   is_sp <- x$CATEGORY %in% taxa
   is_obs <- is.na(x$BREEDING.CODE) |
