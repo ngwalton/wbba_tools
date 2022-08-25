af6b65728bf25e2ffd80677050afc9b7457fab2d
Nick Anich <47158373+nmanich@users.noreply.github.com>
2022-08-25 14:06:39 -0500

    fixing order

--- analysis/blk_n_map.R
+++ analysis/blk_n_map.R
@@ -81,15 +81,15 @@ sp <- read.delim("ebird_data_sample_wbbaii.txt", quote = "", as.is = TRUE)
 # sample WBBA I from eBird
 sp1 <- read.delim("ebird_data_sample_wbbai.txt", quote = "", as.is = TRUE)
 
-# limit to just atlas data
-sp  <- sp[sp$PROJECT.CODE == "EBIRD_ATL_WI", ]
-sp1  <- sp1[sp1$PROJECT.CODE == "EBIRD_ATL_WI", ]
-
 # fix column names if any are snake_case
 names(sp) <- toupper(names(sp))
 colnames(sp) <- str_replace_all(colnames(sp), "[:punct:]", ".")
 names(sp1) <- toupper(names(sp))
 colnames(sp1) <- str_replace_all(colnames(sp1), "[:punct:]", ".")
+
+# limit to just atlas data
+sp  <- sp[sp$PROJECT.CODE == "EBIRD_ATL_WI", ]
+sp1  <- sp1[sp1$PROJECT.CODE == "EBIRD_ATL_WI", ]
 
 # point count data
 pt_count <- read_excel("point_count_data_sample_wbbaii.xlsx",
